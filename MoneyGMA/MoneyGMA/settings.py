9ac01cf6df12efabbfab72b50367c99dc0dbbfb6
geniusbat-Tytan <43751753+geniusbat@users.noreply.github.com>
2022-06-11 23:25:30 +0200

    FF

--- MoneyGMA/MoneyGMA/settings.py
+++ MoneyGMA/MoneyGMA/settings.py
@@ -144,10 +144,18 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static\\'),
 ]
 
+STATIC_ROOT = [
+    os.path.join(BASE_DIR, 'static_root'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://moneygmav1.herokuapp.com/'
+]
 
 import django_heroku
 django_heroku.settings(locals())

2301069ff1086b3229cd487e2387209ec941545e
geniusbat-Tytan <43751753+geniusbat@users.noreply.github.com>
2022-06-11 23:32:27 +0200

    F

--- MoneyGMA/MoneyGMA/settings.py
+++ MoneyGMA/MoneyGMA/settings.py
@@ -141,7 +141,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static\\'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 STATIC_ROOT = [

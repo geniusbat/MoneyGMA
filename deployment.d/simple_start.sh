51fc717dea9b9951404100d63861efd4833a6a55
geniusbat-Tytan <43751753+geniusbat@users.noreply.github.com>
2024-09-13 12:35:25 +0200

    Simple deployment

--- deployment.d/simple_start.sh
+++ deployment.d/simple_start.sh
@@ -3,4 +3,4 @@ cd ..
 python3 MoneyGMA/manage.py makemigrations
 python3 MoneyGMA/manage.py migrate
 python3 MoneyGMA/manage.py migrate --run-syncdb
-gunicorn gunicorn --bind 127.0.0.1:8122 --error-logfile gunicorn-access.log MoneyGMA.wsgi:application --daemon
+gunicorn --bind 127.0.0.1:8122 --error-logfile gunicorn-access.log MoneyGMA/MoneyGMA.wsgi:application --daemon
